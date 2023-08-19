062562d6e0bf2403ee5bb24ecf09cc50dbcbd9f9
Benjia123 <adeoul001@gmail.com>
2023-08-19 10:19:40 +0100

    Auth and loading screens

--- src/protectedRoute.js
+++ src/protectedRoute.js
@@ -1,7 +1,6 @@
-import { Navigate } from "react-router-dom";
 import { useUserAuth } from "./context/authProvider";
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import Lottie from "lottie-react";
 import LoadingMap from "src/pages/loading_animation.json";
 
@@ -9,19 +8,28 @@ const ProtectedRoute = ({ children }) => {
   const { user } = useUserAuth();
   const location = useLocation();
   const [userLoading, setUserLoading] = useState(true);
+  const navigate = useNavigate();
 
-  setTimeout(() => {
-    if (
-      user === null &&
-      (location.pathname.split("/")[1] === "dashboard" ||
-        location.pathname.split("/")[1] === "")
-    ) {
-      setUserLoading(false);
-      return <Navigate to="/auth/signup" />;
-    } else {
-      setUserLoading(false);
+  useEffect(() => {
+    console.log(user);
+    setTimeout(() => {
+      if (
+        user === null &&
+        (location.pathname.split("/")[1] === "dashboard" ||
+          location.pathname.split("/")[1] === "")
+      ) {
+        setUserLoading(false);
+      } else {
+        setUserLoading(false);
+      }
+    }, 1000);
+  }, [location.pathname, navigate, user]);
+
+  useEffect(() => {
+    if (!user && userLoading) {
+      navigate("/auth/signup");
     }
-  }, 1000);
+  });
 
   return (
     <>
