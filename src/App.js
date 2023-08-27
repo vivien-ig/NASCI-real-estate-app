d42f91f9a9f4033a94fb8f4ce8614071c13c8540
Benjia123 <adeoul001@gmail.com>
2023-08-27 16:48:10 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,38 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { AuthProvider } from "./context/authProvider";
+import { useEffect, useState, Suspense } from "react";
+import Lottie from "lottie-react";
+import Loading from "src/pages/loading_animation.json";
+
 function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    window.setTimeout(() => setIsReady(true), 400);
+  }, []);
+
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      {isReady ? (
+        <Suspense
+          fallback={
+            <Lottie
+              className="startup_loading_screen"
+              animationData={Loading}
+              loop={true}
+            />
+          }
+        >
+          <RouterProvider router={router} />
+        </Suspense>
+      ) : (
+        <Lottie
+          className="startup_loading_screen"
+          animationData={Loading}
+          loop={true}
+        />
+      )}
     </AuthProvider>
   );
 }
--- src/protectedRoute.js
+++ src/protectedRoute.js
@@ -1,52 +1,17 @@
 import { useUserAuth } from "./context/authProvider";
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import Lottie from "lottie-react";
-import LoadingMap from "src/pages/loading_animation.json";
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const { user } = useUserAuth();
-  const location = useLocation();
-  const [userLoading, setUserLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log(user);
-    setTimeout(() => {
-      if (
-        user === null &&
-        (location.pathname.split("/")[1] === "dashboard" ||
-          location.pathname.split("/")[1] === "")
-      ) {
-        setUserLoading(false);
-      } else {
-        setUserLoading(false);
-      }
-    }, 1000);
-  }, [location.pathname, navigate, user]);
-
-  useEffect(() => {
-    if (!user && userLoading) {
+    if (!user) {
       navigate("/auth/signup");
     }
   });
 
-  return (
-    <>
-      {!userLoading ? (
-        children
-      ) : (
-        <>
-          {" "}
-          <Lottie
-            className="startup_loading_screen"
-            animationData={LoadingMap}
-            loop={true}
-          />
-        </>
-      )}
-    </>
-  );
+  return children;
 };
 
 export default ProtectedRoute;
